847c14774a6ed969bd874e9001901cf63255fd28
Onni Nikku <onni.nikku@gmail.com>
2022-10-18 13:53:22 +0300

    cookies and sessions work

--- bank_frontend/src/App.js
+++ bank_frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 
 function App() {
@@ -10,6 +10,8 @@ function App() {
   const [password, setPassword] = useState('')
 
   const [loginStatus, setLoginStatus] = useState('')
+
+  Axios.defaults.withCredentials = true;
 
   const register = () => {
     Axios.post('http://localhost:3306/register', {username: usernameReg, password: passwordReg,
@@ -30,6 +32,14 @@ function App() {
     
   });
   };
+
+  useEffect(()=> {
+    Axios.get("http://localhost:3306/login").then((response) => {
+    if (response.data.loggedIn == true) {    
+    setLoginStatus(response.data.user[0].username);
+    }
+    });
+  }, []);
 
 
   return (
--- server/index.js
+++ server/index.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    key: "userId",
+    secret: "bank",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60*60*24
+    },
+})
+);
 
 const db = mysql.createConnection({
     user: "root",
@@ -40,6 +59,15 @@ app.post('/register', (req, res)=> {
     
 });
 
+
+app.get("/login", (req, res) => {
+    if(req.session.user) {
+        res.send({loggedIn: true, user: req.session.user});
+    } else {
+        res.send({loggedIn: false});
+    }
+});
+
 app.post('/login', (req,res)=> {
 
     const username = req.body.username
@@ -57,6 +85,8 @@ app.post('/login', (req,res)=> {
         if (result.length > 0) {
             bcrypt.compare(password, result[0].password, (error, response) => {
                 if (response) {
+                    req.session.user = result;
+                    console.log(req.session.user);
                     res.send(result);
                 } else {
                     res.send({message: "Wrong username/password!"});
